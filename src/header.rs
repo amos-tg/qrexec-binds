e62229fe74af4852ead4cc08755a3b79f047ee80
amos-tg <morphouswatery@gmail.com>
2025-09-09 16:20:21 -0400

    read and write fn meat to stop perma blocking
    the read and write functions have been updated as well as the Qrexec
    struct itself in order to stop infinite blocking on read calls.

--- src/header.rs
+++ src/header.rs
@@ -1,23 +1,23 @@
-//use std::ops::{Deref, DerefMut};
-
 pub(crate) type Header = [u8; 8];
 pub(crate) const HEADER: Header = [0u8; 8];  
 pub(crate) const HEADER_LEN: usize = HEADER.len();
 
+/// the header does not include the length of itself
 pub(crate) fn header(data_buf: &[u8]) -> Header {
     let blen = data_buf.as_ref().len();
 
-    #[cfg(target_endian="big")]
+    #[cfg(target_endian = "big")]
     return blen.to_be_bytes();
 
-    #[cfg(target_endian="little")]
+    #[cfg(target_endian = "little")]
     return blen.to_le_bytes();
 }
 
-pub(crate) fn header_len(header: &Header) -> u64 {
+/// the header does not include the length of itself
+pub(crate) fn header_len(header: &Header) -> usize {
     #[cfg(target_endian = "big")]
     return u64::from_be_bytes(header.clone());
 
     #[cfg(target_endian = "little")]
-    return u64::from_be_bytes(header.clone());
+    return (u64::from_be_bytes(header.clone())) as usize;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#[cfg(target_arch = "x86_64")]
+
 pub mod errors;
 mod header;
 
@@ -13,39 +15,74 @@ use std::{
     },
 };
 use anyhow::anyhow;
-use crate::errors::*;
+use crate::{
+    errors::*,
+    header::*,
+};
 
+pub enum Model {
+    Client,
+    Server,
+}
+
+/// WRITE_BUF_SIZE is the size of the buffer used with the 
+/// qrexec-client-vm call, --buffer-size=WRITE_BUF_SIZE, argument, 
+/// and the size of the buffer used to write into the 
+/// qrexec-client-vm file descriptors behind the scenes. 
+/// The only thing you need to know when you set this is that 
+/// 8 extra bytes are taken up by the header therefore you cannot 
+/// send more data than <WRITE_BUF_SIZE - 8> in a single write call.  
 #[derive(Debug)]
-pub struct Qrexec { 
+pub struct Qrexec<const WRITE_BUF_SIZE: usize> { 
+    wbuf: [u8; WRITE_BUF_SIZE],
     pub child: Child,
     pub stdout: ChildStdout,
     pub stdin: ChildStdin,
     pub stderr: ChildStderr,
 }
 
-impl Qrexec {
+impl<const WRITE_BUF_SIZE: usize> Qrexec<WRITE_BUF_SIZE> {
     /// Calls qrexec-client-vm with the arguments provided through the args parameter.
+    /// Arguments:
     ///
-    /// # Examples 
+    /// target_vmname: 
+    /// self explanatory 
     ///
-    /// ```
-    /// let qrx = Qrexec::new(&[
-    ///     "--buffer-size=10",
-    ///     "target_vmname",
-    ///     "rpc_service_on_target_vmname",
-    /// ]);
+    /// rpc_service: 
+    /// the service you are calling on the target vm, this can include 
+    /// an argument for the service using this syntax: some.service+argument. 
     ///
-    /// Qrexec::write(&mut qrx.stdin, &[0, 1, 2])?;
-    /// ```
-    pub fn new(args: &[&str]) -> QRXRes<Self> {
-
-        let mut child = Command::new("qrexec-client-vm")
-            .args(args)
-            .stdout(Stdio::piped())
+    /// local_program: 
+    /// Full path to local program to be connected with remote service.
+    ///
+    /// local_program_args: 
+    /// Arguments for the local program.
+    pub fn new(
+        target_vmname: &str, 
+        rpc_service: &str,
+        local_program: Option<&str>,
+        local_program_args: Option<&[&str]>,
+    ) -> QRXRes<Self> {
+        let mut child = Command::new("qrexec-client-vm");
+        child.stdout(Stdio::piped())
             .stdin(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()?;
+            .stderr(Stdio::piped());
+
+        child.args([
+            &WRITE_BUF_SIZE.to_string(), target_vmname, rpc_service]);
+
+        if let Some(local_program) = local_program {
+            child.arg(local_program);
+        }
+
+        if let Some(args) = local_program_args {
+            child.args(args);
+        }
+
+        let mut child = child.spawn()?;
+
         return Ok(Self {
+            wbuf: [0u8; WRITE_BUF_SIZE],
             stdout: child.stdout.take().ok_or(
                 anyhow!(STDOUT_ERR))?,
             stdin: child.stdin.take().ok_or(
@@ -55,37 +92,51 @@ impl Qrexec {
             child,
         })
     }
-
+    
     /// returns the number of bytes read into the buffer 
-    /// currently this is a direct inlined call the  
-    /// standard libraries read function. 
     #[inline(always)]
-    pub fn read(
-        read: &mut impl Read,
-        buf: &mut [u8],
-    ) -> Result<usize, io::Error> {
-        match read.read(buf) {
-            Ok(nb) => Ok(nb),
-            Err(e) => Err(e),
-        }
+    pub fn read(read: &mut impl Read, buf: &mut [u8]) -> io::Result<usize> {
+        let mut hbuf = [0u8; HEADER_LEN];
+        read.read_exact(&mut hbuf)?;
+
+        let msg_len = header_len(&hbuf) as usize;
+        read.read_exact(&mut buf[..msg_len])?;
+
+        return Ok(msg_len);
     } 
 
-    /// returns the number of bytes written into the buffer
-    /// currently this is a direct inlined call the  
-    /// standard libraries read function. 
+    /// returns the number of bytes written from the buffer.
+    /// You cannot send more data than WRITE_BUF_SIZE - 8 in a 
+    /// single call to this function as this would result in an  
+    /// overflow. 
     #[inline(always)]
-    pub fn write(
-        written: &mut impl Write,
-        buf: &[u8],
-    ) -> Result<usize, io::Error> {
-        match written.write(buf) {
-            Ok(nb) => Ok(nb),
-            Err(e) => Err(e),
+    pub fn write(&mut self, written: &mut impl Write, buf: &[u8]) -> QRXRes<usize> {
+        let total_nb = buf.len() + HEADER_LEN;
+        if (total_nb) > WRITE_BUF_SIZE {
+            Err(anyhow!(WBUF_LEN_ERR))?;
+        } 
+
+        let mut i = 0;
+        let header = header(buf);
+
+        for val in header {
+            self.wbuf[i] = val;
+            i += 1;
         }
+
+        i = HEADER_LEN;
+        for val in buf {
+            self.wbuf[i] = *val;
+            i += 1;
+        }
+
+        written.write_all(&self.wbuf[..i])?;
+
+        return Ok(total_nb);
     }
 }
 
-impl Drop for Qrexec {
+impl<const WRITE_BUF_SIZE: usize> Drop for Qrexec<WRITE_BUF_SIZE> {
     fn drop(&mut self) {
         let _ = self.child.kill();   
     }
