d789e5b478a06f1ee13da6faea7191208bbf89c1
amos-tg <morphouswatery@gmail.com>
2025-09-09 23:53:37 -0400

    Server support & QIO trait.
    I realized that I had written myself into a corner on the server side
    because the Qrexec struct which contained the write buffer only existed
    on the client side; I rectified this situation by writing a struct for
    the server side as well. This removed the need for genericism on the
    read and write methods so read and write are now cleaner methods on the
    structs for both sides of the vchan.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,13 @@ pub mod errors;
 mod header;
 
 use std::{
-    io::{Write, Read, self},
+    io::{
+        Write, 
+        Read,
+        Stdin,
+        Stdout,
+        self,
+    },
     process::{
         Command,
         Child,
@@ -20,28 +26,103 @@ use crate::{
     header::*,
 };
 
-pub enum Model {
-    Client,
-    Server,
+pub trait QIO {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize>;
+    fn write(&mut self, buf: &[u8]) -> QRXRes<usize>;
 }
 
-/// WRITE_BUF_SIZE is the size of the buffer used with the 
-/// qrexec-client-vm call, --buffer-size=WRITE_BUF_SIZE, argument, 
+/// returns the number of bytes read into the buffer 
+#[inline(always)]
+fn inner_read(read: &mut impl Read, buf: &mut [u8]) -> io::Result<usize> {
+    let mut hbuf = [0u8; HEADER_LEN];
+    read.read_exact(&mut hbuf)?;
+
+    let msg_len = header_len(&hbuf) as usize;
+    read.read_exact(&mut buf[..msg_len])?;
+
+    return Ok(msg_len);
+} 
+
+/// returns the number of bytes written from the buffer.
+/// You cannot send more data than BUF_LEN - 8 in a 
+/// single call to this function as this would result in an  
+/// overflow. 
+#[inline(always)]
+fn inner_write<const BUF_LEN: usize>(
+    wbuf: &mut [u8; BUF_LEN],
+    written: &mut impl Write,
+    data_buf: &[u8],
+) -> QRXRes<usize> {
+    let total_nb = data_buf.len() + HEADER_LEN;
+    if (total_nb) > BUF_LEN {
+        Err(anyhow!(WBUF_LEN_ERR))?;
+    } 
+
+    let mut i = 0;
+    let header = header(data_buf);
+
+    for val in header {
+        wbuf[i] = val;
+        i += 1;
+    }
+
+    i = HEADER_LEN;
+    for val in data_buf {
+        wbuf[i] = *val;
+        i += 1;
+    }
+
+    written.write_all(&wbuf[..i])?;
+
+    return Ok(total_nb);
+}
+
+pub struct QrexecServer<const BUF_LEN: usize> {
+    wbuf: [u8; BUF_LEN], 
+    read: Stdin,
+    written: Stdout,
+}
+
+impl<const BUF_LEN: usize> QIO for QrexecServer<BUF_LEN> {
+    #[inline(always)]
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        return inner_read(&mut self.read, buf);
+    }
+
+    #[inline(always)]
+    fn write(&mut self, buf: &[u8]) -> QRXRes<usize> {
+        return inner_write(&mut self.wbuf, &mut self.written, buf);
+    }
+}
+
+impl<const BUF_LEN: usize> QrexecServer<BUF_LEN> {
+    pub fn new() -> Self {
+        let read = io::stdin();  
+        let written = io::stdout();
+        let wbuf = [0u8; BUF_LEN];
+        Self {
+            wbuf, read, written,
+        }
+    } 
+}
+
+/// BUF_LEN is the size of the buffer used with the 
+/// qrexec-client-vm call, --buffer-size=BUF_LEN, argument, 
 /// and the size of the buffer used to write into the 
 /// qrexec-client-vm file descriptors behind the scenes. 
 /// The only thing you need to know when you set this is that 
 /// 8 extra bytes are taken up by the header therefore you cannot 
-/// send more data than <WRITE_BUF_SIZE - 8> in a single write call.  
+/// send more data than <BUF_LEN - 8> in a single write call.  
 #[derive(Debug)]
-pub struct Qrexec<const WRITE_BUF_SIZE: usize> { 
-    wbuf: [u8; WRITE_BUF_SIZE],
+pub struct QrexecClient<const BUF_LEN: usize> { 
+    wbuf: [u8; BUF_LEN],
     pub child: Child,
-    pub stdout: ChildStdout,
-    pub stdin: ChildStdin,
+    pub read: ChildStdout,
+    pub written: ChildStdin,
     pub stderr: ChildStderr,
 }
 
-impl<const WRITE_BUF_SIZE: usize> Qrexec<WRITE_BUF_SIZE> {
+impl<const BUF_LEN: usize> QrexecClient<BUF_LEN> {
     /// Calls qrexec-client-vm with the arguments provided through the args parameter.
     /// Arguments:
     ///
@@ -69,7 +150,7 @@ impl<const WRITE_BUF_SIZE: usize> Qrexec<WRITE_BUF_SIZE> {
             .stderr(Stdio::piped());
 
         child.args([
-            &WRITE_BUF_SIZE.to_string(), target_vmname, rpc_service]);
+            &BUF_LEN.to_string(), target_vmname, rpc_service]);
 
         if let Some(local_program) = local_program {
             child.arg(local_program);
@@ -82,61 +163,31 @@ impl<const WRITE_BUF_SIZE: usize> Qrexec<WRITE_BUF_SIZE> {
         let mut child = child.spawn()?;
 
         return Ok(Self {
-            wbuf: [0u8; WRITE_BUF_SIZE],
-            stdout: child.stdout.take().ok_or(
+            wbuf: [0u8; BUF_LEN],
+            read: child.stdout.take().ok_or(
                 anyhow!(STDOUT_ERR))?,
-            stdin: child.stdin.take().ok_or(
+            written: child.stdin.take().ok_or(
                 anyhow!(STDIN_ERR))?,
             stderr: child.stderr.take().ok_or(
                 anyhow!(STDERR_ERR))?,
             child,
         })
     }
-    
-    /// returns the number of bytes read into the buffer 
+}
+
+impl<const BUF_LEN: usize> QIO for QrexecClient<BUF_LEN> {
     #[inline(always)]
-    pub fn read(read: &mut impl Read, buf: &mut [u8]) -> io::Result<usize> {
-        let mut hbuf = [0u8; HEADER_LEN];
-        read.read_exact(&mut hbuf)?;
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        return inner_read(&mut self.read, buf);
+    }  
 
-        let msg_len = header_len(&hbuf) as usize;
-        read.read_exact(&mut buf[..msg_len])?;
-
-        return Ok(msg_len);
-    } 
-
-    /// returns the number of bytes written from the buffer.
-    /// You cannot send more data than WRITE_BUF_SIZE - 8 in a 
-    /// single call to this function as this would result in an  
-    /// overflow. 
     #[inline(always)]
-    pub fn write(&mut self, written: &mut impl Write, buf: &[u8]) -> QRXRes<usize> {
-        let total_nb = buf.len() + HEADER_LEN;
-        if (total_nb) > WRITE_BUF_SIZE {
-            Err(anyhow!(WBUF_LEN_ERR))?;
-        } 
-
-        let mut i = 0;
-        let header = header(buf);
-
-        for val in header {
-            self.wbuf[i] = val;
-            i += 1;
-        }
-
-        i = HEADER_LEN;
-        for val in buf {
-            self.wbuf[i] = *val;
-            i += 1;
-        }
-
-        written.write_all(&self.wbuf[..i])?;
-
-        return Ok(total_nb);
+    fn write(&mut self, buf: &[u8]) -> QRXRes<usize> {
+        return inner_write(&mut self.wbuf, &mut self.written, buf);
     }
 }
 
-impl<const WRITE_BUF_SIZE: usize> Drop for Qrexec<WRITE_BUF_SIZE> {
+impl<const BUF_LEN: usize> Drop for QrexecClient<BUF_LEN> { 
     fn drop(&mut self) {
         let _ = self.child.kill();   
     }
